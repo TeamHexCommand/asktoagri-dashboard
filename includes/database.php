175ff5e914a611bbc0eca418fd9f091993424b40
Yash Gohel <yashgohel16@gmail.com>
2022-10-23 01:11:29 +0530

    db port

--- includes/database.php
+++ includes/database.php
@@ -4,8 +4,9 @@ $host = $_SERVER['DB_HOST'];
 $user = $_SERVER['DB_USER'];
 $pass = $_SERVER['DB_PASSWORD'];
 $db   = $_SERVER['DB_DATABASE'];
+$port = $_SERVER['DB_PORT'];
 
-$con = mysqli_connect($host, $user, $pass, $db);
+$con = mysqli_connect($host, $user, $pass, $db, $port);
 
 // Check connection
 if (mysqli_connect_errno()) {
